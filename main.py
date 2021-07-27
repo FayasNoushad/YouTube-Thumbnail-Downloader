9f0c16762f62f8466dad05ecc44fc5e0d8e1ebc9
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-27 14:54:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,18 @@ Bot = Client(
     api_hash = os.environ["API_HASH"]
 )
 
+@Bot.on_message(filters.private & filters.text)
+async def send_thumbnail(bot, update):
+    if ("youtube.com" in update.text) and ("/" in update.text) and ("=" in update.text):
+        id = update.text.split("=", -1)[1]
+    elif ("youtu.be" in update.text) and ("/" in update.text):
+        id = update.text.split("/", -1)[1]
+    else:
+        id = update.text
+    try:
+        thumbnail = "https://i.ytimg.com/vi/" + id + "maxresdefault.jpg"
+        await update.reply_photo(thumbnail)
+    except Exception as error:
+        await update.reply_text(error)
+
 Bot.run()
