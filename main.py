a6a4d5629e310a54ced50f80ef4f2bd71131a42b
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-07-27 14:43:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 import os
 import requests
 from pyrogram import Client, filters
+
+
+Bot = Client(
+    "YouTube-Thumbnail-Downloader",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+Bot.run()
